2a5102148f38c3d89534fe94c0fc5b780777522e
Tyler Bradley <tyler.bradley@phila.gov>
2020-03-19 16:56:06 -0400

    adding ability to pick dataset and upload custom datasets

--- app.R
+++ app.R
@@ -1,17 +1,10 @@
 library(tidyverse)
 library(shiny)
 library(shinyjs)
-# library(shinythemes)
 library(readxl)
 library(janitor)
 library(shinyalert)
-# library(bsplus)
-
-dat <- readxl::read_excel("data/drbc-deck-1.xlsx") %>%
-  janitor::clean_names() %>%
-  dplyr::mutate(answer = as.character(answer))
-
-# dat <- readr::read_rds("data/adv-r-deck.rds")
+library(waiter)
 
 ui <- tagList(
   useShinyjs(),
@@ -24,56 +17,58 @@ ui <- tagList(
   fluidPage(
     includeCSS("www/styles.css"),
     includeScript("www/button_click.js"),
-    div(
-      id = "links-div",
-      tags$a(
-        icon("github"),
-        href = "https://github.com/tbradley1013/shiny-flash-cards"
+    shinyjs::hidden(
+      div(
+        id = "main-content",
+        div(
+          id = "links-div",
+          actionButton(
+            inputId = "change_dataset",
+            label = "",
+            icon = icon("database"),
+            class = "btn-primary btn-sm"
+          ),
+          tags$a(
+            icon("github"),
+            href = "https://github.com/tbradley1013/shiny-flash-cards"
+          )
+        ),
+        div(
+          h2("Shiny Flash Cards"),
+          h4("An application designed to make memorization easier!"),
+          style = "text-align:center;"
+        ),
+        br(),
+        uiOutput("card"),
+        br(),
+        fluidRow(
+          actionButton(
+            inputId = "know_it",
+            label = "I know it!",
+            class = "btn-success btn-lg",
+            width = "33%" 
+          ),
+          actionButton(
+            inputId = "show_answer",
+            label = "Show Answer",
+            class = "btn-primary btn-lg",
+            width = "33%"
+          ),
+          actionButton(
+            inputId = "next_question",
+            label = "Next Question",
+            class = "btn-danger btn-lg",
+            width = "33%"
+          ),
+          inline = TRUE,
+          style = "width:50%;margin: 0 auto;"
+        ),
+        div(
+          tags$p(tags$kbd("a"), ": Toggle Question/Answer"),
+          tags$p(tags$kbd("d"), ": Next Question"),
+          tags$p(tags$kbd("w"), ": I Know it!")
+        )
       )
-    ),
-    div(
-      h2("Shiny Flash Cards"),
-      h4("An application designed to make memorization easier!"),
-      style = "text-align:center;"
-    ),
-    # div(
-    #   actionButton(
-    #     inputId = "show_answer",
-    #     label = "Show Answer",
-    #     class = "btn-primary btn-lg",
-    #     width = "100%"
-    #   ),
-    #   style = "width:200px;margin:10px auto 0;"
-    # ),
-    br(),
-    uiOutput("card"),
-    br(),
-    fluidRow(
-      actionButton(
-        inputId = "know_it",
-        label = "I know it!",
-        class = "btn-success btn-lg",
-        width = "33%" 
-      ),
-      actionButton(
-        inputId = "show_answer",
-        label = "Show Answer",
-        class = "btn-primary btn-lg",
-        width = "33%"
-      ),
-      actionButton(
-        inputId = "next_question",
-        label = "Next Question",
-        class = "btn-danger btn-lg",
-        width = "33%"
-      ),
-      inline = TRUE,
-      style = "width:50%;margin: 0 auto;"
-    ),
-    div(
-      tags$p(tags$kbd("a"), ": Toggle Question/Answer"),
-      tags$p(tags$kbd("d"), ": Next Question"),
-      tags$p(tags$kbd("w"), ": I Know it!")
     )
   )
 )
@@ -82,10 +77,100 @@ ui <- tagList(
 server <- function(input, output, session){
   session$onSessionEnded(stopApp)
   
+  dialog <- modalDialog(
+    h1("Select Dataset!"),
+    selectInput(
+      inputId = "dataset_choice",
+      label = "Datasets",
+      choices = c(
+        "Advanced R" = "adv_r",
+        "Delaware River Watershed" = "drbc",
+        "Custom" = "custom"
+      ),
+      selected = "adv_r"
+    ),
+    uiOutput("custom_data"),
+    footer = tagList(
+      actionButton(
+        inputId = "load_dataset",
+        label = "Load Flash Cards!",
+        class = "btn-primary"
+      )
+    )
+  )
+  
+  observe({
+    showModal(dialog)
+  })
+  
+  output$custom_data <- renderUI({
+    req(input$dataset_choice)
+    
+    if (input$dataset_choice == "custom"){
+      out <- tagList(
+        fileInput(
+          inputId = "custom_file",
+          label = "Custom File",
+          accept = c("xlsx", "csv", "rds")
+        ),
+        helpText("File must be a `.csv`, `.xlsx`, or `.rds` file with columns named `question` and `answer`.\nThese columns can contain HTML for custom formatting!")
+      )
+    } else out <- NULL
+    
+    return(out)
+  })
+  
+  w <- waiter::Waiter$new()
+  
+  dat <- eventReactive(input$load_dataset, {
+    dat_choice <- input$dataset_choice
+    if (dat_choice == "custom"){
+      dat_file <- input$custom_file
+      dat_ext <- tools::file_ext(dat_file$datapath)
+    } 
+    removeModal()
+    w$show
+    
+    if (dat_choice == "adv_r"){
+      dat <- readr::read_rds("data/adv-r-deck.rds")
+    } else if (dat_choice == "drbc"){
+      dat <- readxl::read_excel("data/drbc-deck-1.xlsx") %>%
+        janitor::clean_names() %>%
+        dplyr::mutate(answer = as.character(answer))
+    } else {
+      if (dat_ext == "xlsx"){
+        dat <- readxl::read_excel(dat_file$datapath) %>% 
+          janitor::clean_names()
+      } else if (dat_ext == "csv"){
+        dat <- readr::read_csv(dat_file$datapath) %>% 
+          janitor::clean_names()
+      } else if (dat_ext == "rds"){
+        dat <- readr::read_rds(dat_file$datapath) %>% 
+          janitor::clean_names()
+      } else {
+        showModal(dialog)
+        return(NULL)
+      }
+      
+      if (!all(c("question", "answer") %in% colnames(dat))){
+        showModal(dialog)
+        return(NULL)
+      }
+    }
+    
+    shinyjs::show("main-content")
+    w$hide
+    
+    return(dat)
+  })
+  
+  observeEvent(input$change_dataset, {
+    shinyjs::hide("main-content")
+    showModal(dialog)
+  })
+  
+  
   rv <- reactiveValues(
-    dat = dat,
-    n_cards = length(unique(dat$question)),
-    # card_idx = 1:length(unique(dat$question)),
     answer_visible = FALSE,
     question_visible = TRUE,
     card_keep = numeric(0),
@@ -93,12 +178,14 @@ server <- function(input, output, session){
   )
   
   observe({
+    rv$n_cards <- length(unique(dat()$question))
     rv$card_idx <- sample(1:rv$n_cards, rv$n_cards)
     rv$n <- 1
   })
   
   card_html <- reactive({
-    rv$dat %>% 
+    dat() %>% 
+      select(question, answer) %>% 
       group_nest(question, .key = "answer") %>% 
       mutate(
         question = purrr::map(question, ~{
